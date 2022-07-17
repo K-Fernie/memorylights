6f6037a46b77b49148b364d9ac27f2e6e1d31240
Katy Fernelius <kkin001@gmail.com>
2022-07-16 22:43:38 -0500

    Game play start button working

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
-let userClicks = [];
+const startBtn = document.getElementById("start_btn");
+const gameInstruction = document.getElementsByTagName("header")[0];
 
+let userClicks = [];
+const randArray3 = [
+  Math.floor(Math.random() * 9),
+  Math.floor(Math.random() * 9),
+  Math.floor(Math.random() * 9),
+];
 //window.onload = function;
 function bulbCreation() {
-  for (i = 0; i < 9; i++) {
-    const bulbSection = document.getElementById("bulb_section");
-    //create new Div
-    var newDiv = document.createElement("div");
-    newDiv.setAttribute("class", "light_container");
-    newDiv.innerHTML = `<span class="material-symbols-outlined light_bulb">lightbulb</span>`;
-    newDiv.addEventListener("click", () => {
-      console.log(newDiv);
-    });
-    //append new div to the bulb section
-    bulbSection.appendChild(newDiv);
-  }
+  const bulbSection = document.getElementById("bulb_section");
+  //create new Div
+  var newDiv = document.createElement("div");
+  newDiv.setAttribute("class", "light_container");
+  newDiv.setAttribute("id", `${i}`);
+  newDiv.innerHTML = `<span class="material-symbols-outlined light_bulb">lightbulb</span>`;
+
+  newDiv.addEventListener("click", () => {
+    if (userClicks.length < 3) {
+      userClicks.push(Number(newDiv.getAttribute("id")));
+    }
+    console.log(userClicks);
+  });
+  //append new div to the bulb section
+  bulbSection.appendChild(newDiv);
 }
 
 function round1() {
+  gameInstruction.innerText = "Watch the lights closely...";
   const allLights = document.getElementsByClassName("light_bulb");
   //allLights.item(1).style.color = "yellow";
-  const randArray3 = [
-    Math.floor(Math.random() * 9),
-    Math.floor(Math.random() * 9),
-    Math.floor(Math.random() * 9),
-  ];
 
   randArray3.forEach((element, i) => {
     setTimeout(() => {
+      console.log(element);
       let bulb = allLights.item(element);
       bulb.style.color = "LightYellow";
 
@@ -35,7 +42,15 @@ function round1() {
       }, 600);
     }, i * 1000);
   });
+
+  setTimeout(() => {
+    gameInstruction.innerText = "Click the lights in the order they lit up";
+  }, 3000);
 }
 
-bulbCreation();
-round1();
+//creating all the bulb elements
+for (i = 0; i < 9; i++) {
+  bulbCreation();
+}
+
+startBtn.addEventListener("click", round1);
